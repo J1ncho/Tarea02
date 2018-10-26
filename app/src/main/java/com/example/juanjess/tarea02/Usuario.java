9562abed5f5113e98954c0b2f10d83689a141da6
J1ncho <juanjesusvch@gmail.com>
2018-10-25 20:24:41 -0500

    commit 2

---
+++ app/src/main/java/com/example/juanjess/tarea02/Usuario.java
@@ -0,0 +1,20 @@
+package com.example.juanjess.tarea02;
+
+public class Usuario {
+    public int img;
+    public String nombre;
+    public String telefono;
+    public String correo;
+
+    public Usuario(){
+        super();
+    }
+
+    public Usuario(int img, String nombre, String telefono, String correo) {
+        super();
+        this.img = img;
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.correo = correo;
+    }
+}
