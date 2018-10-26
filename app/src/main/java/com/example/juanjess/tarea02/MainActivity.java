4ad85743faa738243b5f310cb8decab8b4de63e0
J1ncho <juanjesusvch@gmail.com>
2018-10-25 20:23:38 -0500

    Adicion de array contenedor y vista de item (usuario)

--- app/src/main/java/com/example/juanjess/tarea02/MainActivity.java
+++ app/src/main/java/com/example/juanjess/tarea02/MainActivity.java
@@ -2,12 +2,39 @@ package com.example.juanjess.tarea02;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView Lista;
+    Button Agregar;
+    Button Mostrar;
+    EditText Nombre;
+    EditText Telefono;
+    EditText Correo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Agregar=(Button)findViewById(R.id.btnagregar);
+        Mostrar=(Button)findViewById(R.id.btnmostrar);
+        Nombre=(EditText)findViewById(R.id.txtnombre);
+        Telefono=(EditText)findViewById(R.id.txttelefono);
+        Correo=(EditText)findViewById(R.id.txtcorreo);
+        Usuario lista_usuarios[] = new Usuario[100];
+
+        Agregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(Nombre.getText().toString()!="" && Telefono.getText().toString() !="" && Correo.getText().toString()!="") {
+                    new Usuario(R.drawable.ic_launcher, Nombre.getText().toString(), Telefono.getText().toString(), Correo.getText().toString());
+                }
+            }
+        });
     }
 }
